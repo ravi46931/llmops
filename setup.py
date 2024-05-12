0cd0dbc3d2c063fa12bbeeaa20403a74c6dd7829
Ravi Kumar <ravikumar46931@gmail.com>
2024-05-12 06:16:29 +0530

    QASystem with streamlit app

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qasystem",
+    version="0.0.1",
+    author='Ravi',
+    author_email='ravikumar46931@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
+
+##Run the following command
+"""python setup.py install"""
